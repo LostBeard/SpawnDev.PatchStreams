d323eb0341b1bc2d46295074a9d7663121aa62a3
Todd Tanner <todd@spawndev.com>
2024-08-24 22:26:33 -0400

    Added console demo and updated README.md

--- SpawnDev.PatchStreams.DemoConsole/Program.cs
+++ SpawnDev.PatchStreams.DemoConsole/Program.cs
@@ -1,21 +1,4 @@
-# SpawnDev.PatchStreams
-
-[![NuGet version](https://badge.fury.io/nu/SpawnDev.PatchStreams.svg)](https://www.nuget.org/packages/SpawnDev.PatchStreams)
-
-## PatchStream : Stream
-
-- PatchStream inherits from [Stream](https://learn.microsoft.com/en-us/dotnet/api/system.io.stream?view=net-8.0) making it easy to use with countless existing libraries that can work with Streams.
-- It is a readable, writable stream that, when modified, does not modify any source data or data added to it, but instead creates patches to represent the data changes.
-- Supports data deletion, insertion, overwriting, slicing, splicing, undo/redo, restore points, partial data views, multi-stream read-only sources, and multiple stream insertion.
-- All modifications to a PatchStream are saved in patches which can be undone and redone as fast as changing a single integer value
-- Restore points can be set at any point and restored easily to make undo/redo easier.
-- Low memory usage and blazing fast modifications.
-- IMPORTANT - Once data has been added to a PatchStream it should not be modified in any way. Modify the PatchStream itself.
-
-
-The below code is a basic demonstration of PatchStream reading writing, inserting, deleting, restore point usage, and an undo.
-```cs
-using SpawnDev.PatchStreams;
+﻿using SpawnDev.PatchStreams;
 
 // Create a new PatchStream with or without source data.
 // Source data and data added to PatchStream should not be modified once it is added
@@ -62,5 +45,3 @@ patchStream.RestorePointUndo();
 
 // patchStream data is now "Hello world!"
 Console.WriteLine(patchStream.ToString(true));
-
-```
